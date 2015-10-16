6e8f143c589a056d93ca61888ee1a3bfbf36b325
Kelvin Liu <kelvinliu@172-16-168-48.DYNAPOOL.NYU.EDU>
2015-10-16 13:42:38 -0400

    added package

--- src/main/java/com/thekelvinliu/KPCBChallenge/Main.java
+++ src/main/java/com/thekelvinliu/KPCBChallenge/Main.java
@@ -1,8 +1,8 @@
+package com.thekelvinliu.KPCBChallenge;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world!");
-        for (String s : args) {
-            System.out.println(s);
-        }
+        for (String s : args) System.out.println(s);
     }
 }
