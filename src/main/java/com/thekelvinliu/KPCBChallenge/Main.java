5a931cb5022e244a5f22ee69863193158b06cd3d
Kelvin Liu <kelvinliu@172-16-200-232.DYNAPOOL.NYU.EDU>
2015-10-21 15:02:41 -0400

    more quick testing

--- src/main/java/com/thekelvinliu/KPCBChallenge/Main.java
+++ src/main/java/com/thekelvinliu/KPCBChallenge/Main.java
@@ -1,8 +1,37 @@
 package com.thekelvinliu.KPCBChallenge;
 
+import com.thekelvinliu.KPCBChallenge.FixedSizeHashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world!");
-        for (String s : args) System.out.println(s);
+        if (args.length != 1) {
+            System.out.println("include size");
+            System.exit(1);
+        }
+        int s = Integer.parseInt(args[0]);
+        FixedSizeHashMap<Integer> fshmI = new FixedSizeHashMap<Integer>(s);
+        System.out.println(fshmI);
+        // int a = 1;
+        // if (fshmI.set("a", a++)) System.out.printf("The load is %f%n", fshmI.load());
+        // else System.out.println("setting did NOT work.");
+        // if (fshmI.set("b", a++)) System.out.printf("The load is %f%n", fshmI.load());
+        // else System.out.println("setting did NOT work.");
+        // if (fshmI.set("c", a++)) System.out.printf("The load is %f%n", fshmI.load());
+        // else System.out.println("setting did NOT work.");
+        // if (fshmI.set("d", a++)) System.out.printf("The load is %f%n", fshmI.load());
+        // else System.out.println("setting did NOT work.");
+        // System.out.printf("the value that corresponds to 'd' is %d%n", fshmI.get("d"));
+
+        // a = 1;
+        // FixedSizeHashMap<String> fshmS = new FixedSizeHashMap<String>(3);
+        // if (fshmS.set(Integer.toString(a++), "a")) System.out.printf("The load is %f%n", fshmS.load());
+        // else System.out.println("setting did NOT work.");
+        // if (fshmS.set(Integer.toString(a++), "b")) System.out.printf("The load is %f%n", fshmS.load());
+        // else System.out.println("setting did NOT work.");
+        // if (fshmS.set(Integer.toString(a++), "c")) System.out.printf("The load is %f%n", fshmS.load());
+        // else System.out.println("setting did NOT work.");
+        // if (fshmS.set(Integer.toString(a++), "d")) System.out.printf("The load is %f%n", fshmS.load());
+        // else System.out.println("setting did NOT work.");
+        // System.out.printf("the value that corresponds to '4' is '%s'%n", fshmS.get(Integer.toString(4)));
     }
 }
