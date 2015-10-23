462f1c11099f7166b832816d61a1c37be8c3f3f0
Kelvin Liu <kelvinliu@172-16-135-226.DYNAPOOL.NYU.EDU>
2015-10-23 01:19:29 -0400

    created separate tree class for internal structure

--- src/main/java/com/thekelvinliu/KPCBChallenge/FixedSizeHashMap.java
+++ src/main/java/com/thekelvinliu/KPCBChallenge/FixedSizeHashMap.java
@@ -13,17 +13,17 @@ package com.thekelvinliu.KPCBChallenge;
  * @param       <T>     the type of value that will be handled by this hash map.
  */
 public class FixedSizeHashMap<T> {
-    //HELPER CLASS
+    //HELPER CLASSES
     /**
      * A generic class that represents a node in a binary tree.
      *
      * Each node holds a key and a value, as well as references to a node's
-     * parent and left and right children. This class is exclusively available
-     * to FixedSizeHashMap. As such, any fields of any node will be accessed
-     * and modified directly by FixedSizeHashMap.
+     * parent and left and right children. Because this class is not accessible
+     * to the outside world, fields will be accessed and modified directly by
+     * FixedSizeHashMap or AVLTree for convenience.
      * @param       <T>     the type of the value to be held by this node.
      */
-    public class Node<T> {
+    class Node<T> {
         // INSTANCE VARIABLES
         /**
          * This node's key.
@@ -33,6 +33,10 @@ public class FixedSizeHashMap<T> {
          * This node's value.
          */
         public T value;
+        /**
+         * The height of this node inside a tree
+         */
+        public int height;
         /**
          * This node's parent.
          */
@@ -57,19 +61,27 @@ public class FixedSizeHashMap<T> {
         public Node(Node<T> parent) {
             this.key = 0;
             this.value = null;
+            this.height = 0;
             this.parent = parent;
             this.left = null;
             this.right = null;
         }
 
-        /**
-         * Creates a node instance without a parent parameter.
-         */
-        public Node() {
-            this(null);
-        }
-
         //METHODS
+        /**
+         * Returns the height of this node's left child.
+         * @return      the neight of this node's left child
+         */
+        public int lHeight() {
+            return (this.left != null) ? this.left.height : 0;
+        }
+        /**
+         * Returns the height of this node's right child.
+         * @return      the neight of this node's right child
+         */
+        public int rHeight() {
+            return (this.right != null) ? this.right.height : 0;
+        }
         /**
          * Returns index, key, and value information about this node.
          * @return      a string with information about this node.
@@ -78,23 +90,42 @@ public class FixedSizeHashMap<T> {
             return "Node with key " + this.key + " and value " + this.value + ".";
         }
     }
+    /**
+     * A binary search tree made of generic nodes.
+     *
+     * This binary search tree is self-balacing and uses and AVL tree design. It
+     * is used as the internal data structure of the fixed-size hash map.
+     */
+    class AVLTree<T> {
+        //INSTANCE VARIABLES
+        /**
+         * The root of this tree.
+         */
+        private Node<T> root;
+        //CONSTRUCTOR
+        /**
+         * Creates an empty tree.
+         */
+        public AVLTree() {
+            this.root = null;
+        }
+        //METHODS
+    }
 
     //INSTANCE VARIALBES
     /**
      * The root of the internal tree for this fixed-size hash map.
      */
-    private Node<T> tree;
+    private AVLTree<T> tree;
+
     /**
-     * A doubly linked list of nodes not currently active in the tree.
+     * A stack-like doubly linked list used to store unsued nodes.
      *
-     * The head of the list is the first element of the list. A node's next node
-     * is the node's right child. A node's previous node is the node's parent.
+     * The node class is recycled to accomplish this doubly linked list. A
+     * node's next node is the node's right child. A node's previous node is the
+     * node's parent.
      */
     private Node<T> nodeBank;
-    /**
-     * A cursor used for traversing a tree or list of nodes.
-     */
-    private Node<T> cursor;
     /**
      * The maximum number of items this fixed-size hash map can hold.
      */
@@ -112,18 +143,18 @@ public class FixedSizeHashMap<T> {
      * @param       size    the max size of this hash map
      */
     public FixedSizeHashMap(int size) {
-        this.tree = null;
+        this.tree = new AVLTree<T>();
         this.nodeBank = new Node<T>(null);
-        this.cursor = this.nodeBank;
+        Node<T> cur = this.nodeBank;
         for (int i = 1; i < size; i++) {
-            this.cursor.right = new Node<T>(this.cursor);
-            this.cursor = this.cursor.right;
+            cur.right = new Node<T>(cur);
+            cur = cur.right;
         }
         this.size = size;
         this.items = 0;
     }
 
-    //METHODS
+    //USER METHODS (PUBLIC)
     /**
      * Returns a boolean indicating whether key was associated with value.
      *
@@ -144,23 +175,68 @@ public class FixedSizeHashMap<T> {
         }
     }
     /**
-     *
+     * Returns the value corresponding to a given key.
+     * @param       key     the key used to identify the returned value
+     * @return      the value associated with key or null
      */
     public T get(String key) {
         return null;
     }
     /**
-     *
+     * Returns the value corresponding to a given key after deleting the entry.
+     * @param       key     the key used to identify the node to delete
+     * @return      the value of the node deleted or null
      */
     public T delete(String key) {
         return null;
     }
-
     /**
      * Returns the load (ratio of items to size) of this fixed-size hash map.
      * @return      the load of this fixed-size hash map
      */
     public float load() {
         return (float)this.items/this.size;
+    }
+
+    //NODEBANK UTILITY METHODS (PRIVATE)
+    /**
+     * Returns (pops) a node from this.nodeBank.
+     * @return      a node
+     */
+    private Node<T> getNode() {
+        Node<T> retNode = this.nodeBank;
+        this.nodeBank = retNode.right;
+        this.nodeBank.parent = retNode.parent;
+        retNode.right = null;
+        return retNode;
+    }
+    /**
+     * Puts (pushes) a node into this.nodeBank.
+     * @param       node    the node to be put into this.nodeBank
+     */
+    private void putNode(Node<T> node) {
+        node.parent = this.nodeBank.parent;
+        node.right = this.nodeBank;
+        this.nodeBank.parent = node;
+        this.nodeBank = node;
+    }
+
+    //TREE UTILITY METHODS (PRIVATE)
+    /**
+     * Updates the height of the given node based on its children.
+     * @param       n       the node to be updated
+     */
+    private void updateHeight (Node<T> n) {
+        n.height = (n.lHeight() > n.rHeight()) ? n.lHeight() : n.rHeight();
+        n.height++;
+    }
+    /**
+     * Returns whether or not the subtree rooted by the given node is balanced.
+     * @param       n       the node to check
+     * @return      boolean indicating balanced (true) or not balanced (false)
+     */
+    private boolean isBalanced(Node<T> n) {
+        int balFactor = n.lHeight() - n.rHeight();
+        return (-1 <= balFactor && balFactor <= 1) ? true : false;
     }
 }
