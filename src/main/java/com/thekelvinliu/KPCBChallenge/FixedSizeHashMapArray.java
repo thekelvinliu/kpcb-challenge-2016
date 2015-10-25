a614adf19047d54c7e4890c8b1bbb3c03f55cb80
thekelvinliu <kelvin@thekelvinliu.com>
2015-10-25 03:02:43 -0400

    insertion and tree rotations should be working.

--- src/main/java/com/thekelvinliu/KPCBChallenge/FixedSizeHashMapArray.java
+++ src/main/java/com/thekelvinliu/KPCBChallenge/FixedSizeHashMapArray.java
@@ -1,5 +1,7 @@
 package com.thekelvinliu.KPCBChallenge;
 
+import java.util.ArrayDeque;
+
 public class FixedSizeHashMapArray<T> {
     //HELPER CLASS
     public class Node<T> {
@@ -21,18 +23,17 @@ public class FixedSizeHashMapArray<T> {
 
         //METHODS
         public String toString() {
-            String retval = "Node with key " + this.key + " and value ";
-            if (this.value != null) retval += this.value.toString() + ".";
-            else retval += "NULL.";
+            String retval = "(" + this.key + ", ";
+            retval += (this.value != null) ? this.value.toString() + ")" : "NULL)";
             return retval;
         }
     }
 
     //INSTANCE VARIABLES
-    public Node[] tree;
-    public int rootInd;
-    public final int size;
-    public int items;
+    private Node[] tree;
+    private int rootInd;
+    private final int size;
+    private int items;
 
     //CONSTRUCTOR
     public FixedSizeHashMapArray(int size) {
@@ -57,6 +58,7 @@ public class FixedSizeHashMapArray<T> {
                 this.items++;
                 return true;
             } catch (Exception e) {
+                e.printStackTrace();
                 this.cleanNode(this.items);
                 return false;
             }
@@ -67,7 +69,10 @@ public class FixedSizeHashMapArray<T> {
         }
     }
     public T get(String key) {
-        return null;
+        if (this.rootInd == -1)
+            return null;
+        else
+            return null;
     }
     public T delete(String key) {
         return null;
@@ -79,54 +84,84 @@ public class FixedSizeHashMapArray<T> {
     public float load() {
         return (float)this.items/this.size;
     }
+    public int getItemCount() {
+        return this.items;
+    }
+    public int getSize() {
+        return this.size;
+    }
 
     //TREE METHODS
     private int insert(int newInd, int parInd) throws Exception {
         int newParInd = parInd;
         //insert into left subtree
         if (this.tree[newInd].key < this.tree[parInd].key) {
-            if (this.tree[parInd].left == -1)
+            if (this.tree[parInd].left == -1) {
                 this.tree[parInd].left = newInd;
+            }
             else {
                 this.tree[parInd].left = this.insert(newInd, this.tree[parInd].left);
                 if (this.balanceFactor(parInd) == 2) {
-                    if (this.tree[newInd].key < this.tree[this.tree[parInd].left].key)
+                    int lInd = this.tree[parInd].left;
+                    if (lInd != -1 && this.tree[newInd].key < this.tree[lInd].key) {
                         newParInd = this.rotateCaseLeftLeft(parInd);
-                    else
+                    }
+                    else {
                         newParInd = this.rotateCaseLeftRight(parInd);
+                    }
                 }
             }
         //insert into right subtree
         } else if (this.tree[newInd].key > this.tree[parInd].key) {
-            if (this.tree[parInd].right == -1)
+            if (this.tree[parInd].right == -1) {
                 this.tree[parInd].right = newInd;
+            }
             else {
                 this.tree[parInd].right = this.insert(newInd, this.tree[parInd].right);
                 if (this.balanceFactor(parInd) == -2) {
-                    if (this.tree[newInd].key < this.tree[this.tree[parInd].left].key)
+                    int rInd = this.tree[parInd].right;
+                    if (rInd != -1 && this.tree[newInd].key < this.tree[rInd].key) {
                         newParInd = this.rotateCaseRightLeft(parInd);
-                    else
+                    }
+                    else {
                         newParInd = this.rotateCaseRightRight(parInd);
+                    }
                 }
             }
         //bad key
-        } else throw new Exception("Key already used.");
+        } else {
+            throw new Exception("Key already used.");
+        }
         this.updateHeight(parInd);
         return newParInd;
     }
 
     //TREE ROTATIONS
     private int rotateCaseLeftLeft(int startInd) {
-        return startInd;
+        int newStartInd = this.tree[startInd].left;
+        this.tree[startInd].left = this.tree[newStartInd].right;
+        this.tree[newStartInd].right = startInd;
+        //update heights
+        this.updateHeight(startInd);
+        this.updateHeight(newStartInd);
+        return newStartInd;
     }
     private int rotateCaseRightRight(int startInd) {
-        return startInd;
+        int newStartInd = this.tree[startInd].right;
+        this.tree[startInd].right = this.tree[newStartInd].left;
+        this.tree[newStartInd].left = startInd;
+        //update heights
+        this.updateHeight(startInd);
+        this.updateHeight(newStartInd);
+        return newStartInd;
     }
     private int rotateCaseLeftRight(int startInd) {
-        return startInd;
+        this.tree[startInd].left = this.rotateCaseRightRight(this.tree[startInd].left);
+        return this.rotateCaseLeftLeft(startInd);
     }
     private int rotateCaseRightLeft(int startInd) {
-        return startInd;
+        this.tree[startInd].right = this.rotateCaseLeftLeft(this.tree[startInd].right);
+        return this.rotateCaseRightRight(startInd);
     }
 
     //UTILITIES
@@ -146,17 +181,19 @@ public class FixedSizeHashMapArray<T> {
     }
     //returns balance factor of subtree rooted by node at index i
     private int balanceFactor(int i) {
-        return this.tree[i].left - this.tree[i].right;
+        return this.height(this.tree[i].left) - this.height(this.tree[i].right);
     }
     //get height of a node at index i
     private int height(int i) {
-        return (this.tree[i].value != null) ? this.tree[i].height : -1;
+        return (i != -1) ? this.tree[i].height : -1;
     }
     //update the height of a node at index i
     private void updateHeight(int i) {
         int lInd = this.tree[i].left;
         int rInd = this.tree[i].right;
-        if (lInd != -1 && rInd == -1)
+        if (lInd == -1 && rInd == -1)
+            this.tree[i].height = 0;
+        else if (lInd != -1 && rInd == -1)
             this.tree[i].height = this.tree[lInd].height + 1;
         else if (lInd == -1 && rInd != -1)
             this.tree[i].height = this.tree[rInd].height + 1;
@@ -164,14 +201,44 @@ public class FixedSizeHashMapArray<T> {
             this.tree[i].height = this.max(this.height(lInd), this.height(rInd)) + 1;
     }
     //inorder printing of subtree rooted by node at index i
-    public void inorderPrint() {
-        this.inorderPrint(this.rootInd);
+    public void print() {
+        System.out.println();
+        this.preorderPrint(this.rootInd, "");
     }
-    private void inorderPrint(int i) {
+    private void preorderPrint(int i, String indent) {
         if (i >= 0) {
-            this.inorderPrint(this.tree[i].left);
-            System.out.println(this.tree[i]);
-            this.inorderPrint(this.tree[i].right);
+            System.out.println(indent + this.tree[i] + " " + this.height(i));
+            this.preorderPrint(this.tree[i].left, indent + "  ");
+            // if (i == this.rootInd) System.out.print("* ");
+            this.preorderPrint(this.tree[i].right, indent + "  ");
         }
+    }
+    public void bfsPrint() {
+        int x, currentLevel, nextLevel;
+        ArrayDeque<Integer> q = new ArrayDeque<Integer>(this.size);
+        q.add(this.rootInd);
+        currentLevel = 1;
+        nextLevel = 0;
+        while (!q.isEmpty()) {
+            x = q.remove();
+            currentLevel--;
+            System.out.print(" ");
+            if (x != -1) {
+                q.add(this.tree[x].left);
+                q.add(this.tree[x].right);
+                nextLevel += 2;
+                System.out.print(this.tree[x]);
+            }
+            else System.out.print("(  ,  )");
+            System.out.print(" ");
+            if (currentLevel == 0) {
+                currentLevel = nextLevel;
+                nextLevel = 0;
+                System.out.println();
+            }
+        }
+    }
+    public Node<T> getRoot() {
+        return this.tree[this.rootInd];
     }
 }
