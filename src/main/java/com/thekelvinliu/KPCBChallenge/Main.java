59db73adc7207681840164b358f248e88ee2215c
thekelvinliu <kelvin@thekelvinliu.com>
2015-10-25 03:02:29 -0400

    more testing

--- src/main/java/com/thekelvinliu/KPCBChallenge/Main.java
+++ src/main/java/com/thekelvinliu/KPCBChallenge/Main.java
@@ -9,10 +9,15 @@ public class Main {
         }
         int s = Integer.parseInt(args[0]);
         FixedSizeHashMapArray<String> a = new FixedSizeHashMapArray<String>(s);
-        a.set("b", "b");
         a.set("a", "a");
         a.set("c", "c");
+        a.set("d", "d");
+        a.set("b", "b");
+        a.set("f", "f");
+        a.set("e", "e");
+        a.set("_", "_");
+        a.print();
+        // a.bfsPrint();
         System.out.printf("The current load if %f%n", a.load());
-        a.inorderPrint();
     }
 }
